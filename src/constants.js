1de430dd9b8e7d66b5e0ca26919047743e15fc80
Whiffles <68132950+whifflesdev@users.noreply.github.com>
2022-04-13 06:21:53 -0700

    Create constants.js

---
+++ src/constants.js
@@ -0,0 +1,9 @@
+export const CHANNEL_NAME = 'mychannel';
+export const OAUTH_TOKEN = 'oauth:bot-token';
+export const BOT_USERNAME = 'username';
+export const BLOCKED_WORDS = [
+    'f4f',
+    'l4l',
+    'lurk 4 lurk',
+    'lurk for lurk',
+]
